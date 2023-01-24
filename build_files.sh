b2ee64be885a7465d05b018135a6a464dab6086f
dhruvja <dhruv@iamsizzling.com>
2023-01-24 23:19:18 +0530

    some minor changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 echo " BUILD START"
-rm -rf /vercel/path0/requirements.txt
 python3.9 -m pip uninstall psycopg2
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic  --noinput --clear
