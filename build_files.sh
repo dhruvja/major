77c8e14df118fa2d1203372448f0802222fd2a5f
dhruvja <dhruv@iamsizzling.com>
2023-01-24 23:11:20 +0530

    not installing packages

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo " BUILD START"
 python3.9 -m pip uninstall psycopg2
 python3.9 -m pip freeze
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic  --noinput --clear
+
 echo " BUILD END"
