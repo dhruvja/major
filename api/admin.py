312fdc0c3032e164374696255787b818a0e18ea4
dhruvja <dhruv@iamsizzling.com>
2023-04-03 12:03:12 +0200

    added actions for exports

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Admission, AdmissionFile, Result, ResultFile, Placement, PlacementFile
 from more_admin_filters import MultiSelectDropdownFilter
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse, HttpResponseRedirect
+# from pyExcelerator import * 
+# from django.contrib.admin.util import lookup_field
+from django.utils.html import strip_tags
+from django.contrib import messages
+from openpyxl import Workbook
+from django.db import connection
+import xlsxwriter
+# from .actions import export_as_xls
 # from .models import Bos,NewCoursesIntroduced, Consultants, Bookchapter, Seedmoney, Proposal, Journal, Grant,StudentsHigherEducation, AwardsAndRecognistionTeachersStudents, ListMajorMinorResearchProjects, SpecialLectureInCollege, ConferenceAttendedByTeachers, ConferenceConductedInCollege, ProfessionalDevelopmentProg, CollabrativeActivity, FundingStudentProjects, WorkshopAndSeminars, FacultyProfile
 
 from import_export.admin import ImportExportModelAdmin, ExportActionMixin
@@ -12,6 +22,43 @@ class AdmissionAdmin(ImportExportModelAdmin, ExportActionMixin,  admin.ModelAdmi
     list_filter = [("batch", MultiSelectDropdownFilter), ("semester", MultiSelectDropdownFilter)] 
     list_display = ( 'batch', 'semester', 'cet', 'comedk', 'management', 'diploma', 'cob')
     inlines = (AdmissionFileAdminInline, )
+
+    def export (self, request, queryset): 
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM api_admission LEFT JOIN api_placement ON api_admission.batch = api_placement.batch")
+            row = cursor.fetchall()
+            print(row)
+            column_names = []
+            cursor.execute("DESCRIBE api_admission")
+            admission = cursor.fetchall()
+            cursor.execute("DESCRIBE api_placement")
+            placement = cursor.fetchall()
+            for col in admission:
+                column_names.append(col[0])
+            for col in placement:
+                column_names.append(col[0])
+            print(column_names)
+            workbook = xlsxwriter.Workbook('write_list.xlsx')
+            worksheet = workbook.add_worksheet()
+
+            for col_num, data in enumerate(column_names):
+                worksheet.write(0, col_num, data)
+
+            for row_num, row_data in enumerate(row):
+                for col_num, col_data in enumerate(row_data):
+                    worksheet.write(row_num+1, col_num, col_data)
+
+            workbook.close()
+            with open("write_list.xlsx", 'rb') as f:
+                text = f.read()
+                print(text)
+                response = HttpResponse(text, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                response['Content-Disposition'] = 'attachment; filename=write_list.xlsx'
+                return response
+            
+    actions = [export]
+
+    
     class Media:
         js = ('/media/hide_attribute.js',)
 
